f5dedee2503944d441c4479817b0b5b495caccec
jcjessicarmo <jessicascarmo@terra.com.br>
2019-12-11 18:04:44 -0300

    Criar clientes e produtos

---
+++ models/ProdutosSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ProdutosSchema = new Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
+  nome: { type: String, required: true },
+  marca: { type: String, required: true },
+  modelo: { type: String, required: true },
+  descricao: { type: String, required: true },
+  foto: { type: String, required: true },
+  preco: { type: Number },
+
+})
+
+const produtosModel = mongoose.model('produtos', ProdutosSchema);
+
+module.exports = { produtosModel, ProdutosSchema };
